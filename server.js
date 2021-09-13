bf035e0c30a788e07997d265ef50008c6693a67f
Zeafax <fred.sanden@gmail.com>
2021-09-13 14:28:05 +0200

    Intial Author Routes

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const app = express()
 const expressLayouts = require("express-ejs-layouts")
 
 const indexRouter = require("./routes/index")
+const authorRouter = require("./routes/authors")
 
 app.set("view engine", "ejs")
 app.set("views", __dirname + "/views")
 app.set("layout", "layouts/layout")
 app.use(expressLayouts)
 app.use(express.static("public"))
+app.use(express.urlencoded({ limit: "10mb", extended: false}))
 
 
 const mongoose = require("mongoose")
@@ -22,5 +24,6 @@ db.on("error", error => console.error(error))
 db.once("open", () => console.log("Connect succesful to mongoose"))
 
 app.use("/", indexRouter)
+app.use("/authors", authorRouter)
 
 app.listen(process.env.PORT || 3000)
